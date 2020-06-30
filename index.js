818e98e3a4689fa9832c6fde4478181a912fc7db
Eugenia Petrunia <avepetrunia@gmail.com>
2020-06-30 17:16:25 +0300

    Add solution for "reverse" task

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 // 1. Даны два массива: [1, 2, 3] и [4, 5, 6]. Объедините их вместе.
-/**
- *
- * @type {number[]}
- */
-const array1 = [1, 2, 3];
-const array2 = [4, 5, 6];
-const array3 = array1.concat(array2);
+// /**
+//  *
+//  * @type {number[]}
+//  */
+// const array1 = [1, 2, 3];
+// const array2 = [4, 5, 6];
+// const array3 = array1.concat(array2);
 
+// 2. Дан массив [1, 2, 3]. Сделайте из него массив [3, 2, 1].
+// /**
+//  *
+//  * @type {number[]}
+//  */
+// const array = [1, 2, 3];
+// const reversed = array.reverse();
