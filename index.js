134dae6f133ff8ecaf6a23cf6a50e2013ddb3215
Eugenia Petrunia <avepetrunia@gmail.com>
2020-06-30 17:13:43 +0300

    Add solution for "concat" task

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1. Даны два массива: [1, 2, 3] и [4, 5, 6]. Объедините их вместе.
+/**
+ *
+ * @type {number[]}
+ */
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const array3 = array1.concat(array2);
+
